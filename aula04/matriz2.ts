cc159e687aa3bbb28e4ce76f133930e592402f55
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-09-03 15:52:51 -0300

    Create matriz2.ts

---
+++ aula04/matriz2.ts
@@ -0,0 +1,11 @@
+function soma(matriz : number[][]): number{
+    let soma = 0;
+
+    for(let i = 0; i< matriz.length; i++){
+        for(let j = 0; j<matriz[i].length; j++){
+            soma += matriz[i][j];
+        }
+    }
+
+    return soma;
+}
