bf574da6dad17dd9a1a48cabd63c605d5b39a8c4
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-09-06 12:40:46 -0300

    Create questao1.ts

---
+++ listaDeRevisao/questao1.ts
@@ -0,0 +1,9 @@
+const latas: number = 10;
+const copos: number = 5;
+
+if (latas > copos) {
+    console.log('A bandeja foi derrubada e a quantidade de copos quebrados foi:');
+    console.log(copos);
+} else {
+    console.log('A bandeja não será derrubada.');
+}
